cd69c37df19205ea0eec94fdfbea6235fc142b7b
Soham Chitale <chitalesoham106@gmail.com>
2024-12-02 20:26:40 +0530

    added support of hashed passwords for register & login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("./../models/User");
+const bcrypt = require("bcryptjs");
 const { v4: uuidV4 } = require("uuid");
 
 // Register a user
@@ -10,13 +11,15 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
     }
 
-    // create new user with a unique user_id
-    const user = await User.create({
+    const hashedPass = await bcrypt.hash(password, 10);
+    // create new user with a unique user_id and hased password
+    const user = new User({
       username,
       email,
-      password,
+      password: hashedPass,
       user_id: uuidV4(),
     });
+    await user.save();
 
     res.status(201).json({ message: "User created successfully" });
   } catch (err) {
@@ -24,4 +27,22 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+//login a user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userEmailExist = await User.findOne({ email });
+    if (!userEmailExist) {
+      return res.status(401).json({ error: "User not found" });
+    }
+    const isPassValid = await bcrypt.compare(password, userEmailExist.password);
+    if (!isPassValid) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+    res.status(200).json({ message: "User logged in successfully" });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
