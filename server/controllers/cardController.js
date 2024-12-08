51e4c9eb79e2d62dca71a2887f3f8482228a0168
Soham Chitale <chitalesoham106@gmail.com>
2024-12-08 12:57:30 +0530

    card read route added

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -30,4 +30,17 @@ const deleteCard = async(req, res) => {
   }
 }
 
-module.exports = { addCard, deleteCard };
+const getCard = async(req, res) => {
+  const {user_id} = req.query;
+  try{
+    const readCard = await Card.find({user_id});
+    if(!readCard){
+      res.status(404).json({error: "No cards found"});
+    }
+    res.status(200).json({cards: readCard});
+  } catch(err){
+    res.status(500).json({error: err.message});
+  }
+}
+
+module.exports = { addCard, deleteCard, getCard };
--- server/routes/userCard.js
+++ server/routes/userCard.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { addCard, deleteCard } = require("../controllers/cardController");
+const { addCard, deleteCard, getCard } = require("../controllers/cardController");
 
 router.post("/addcard", addCard);
 router.delete("/deletecard", deleteCard);
+router.get("/readcard", getCard);
 
 module.exports = router;
