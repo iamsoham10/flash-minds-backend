9fb1d9efdc9fdd1630d755a571786e683532288a
Soham Chitale <chitalesoham106@gmail.com>
2024-12-11 20:36:00 +0530

    implemented sign up and login feature

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path: "", component: AppComponent },
-    { path: "auth", loadComponent: () => import("./components/signup/signup.component").then((c) => c.SignupComponent) }
+    { path: "auth", loadComponent: () => import("./components/signup/signup.component").then((c) => c.SignupComponent) },
+    { path: "dashboard", loadComponent: () => import("./components/dashboard/dashboard.component").then((c) => c.DashboardComponent) }
 ];
