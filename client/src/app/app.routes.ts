31efe3f9e054a35d61bb5df63abe14229dc3ac3e
Soham Chitale <chitalesoham106@gmail.com>
2024-12-12 23:16:24 +0530

    created dashboard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: "", component: AppComponent },
+    { path: "", loadComponent: () => import("./components/landing-page/landing-page.component").then((c) => c.LandingPageComponent) },
     { path: "auth", loadComponent: () => import("./components/signup/signup.component").then((c) => c.SignupComponent) },
     { path: "dashboard", loadComponent: () => import("./components/dashboard/dashboard.component").then((c) => c.DashboardComponent), canActivate: [authGuard] }
 ];
