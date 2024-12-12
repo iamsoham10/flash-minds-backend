fecb895f37ac36fb4086fa4591dadb96a7ef68bf
Soham Chitale <chitalesoham106@gmail.com>
2024-12-12 17:00:41 +0530

    form ui changes

--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -15,7 +15,7 @@ import { Router } from '@angular/router';
     trigger('floatUpAnimation', [
       transition(':enter', [
         style({ transform: 'translateY(50px)', opacity: 0 }),
-        animate('1s ease-out', style({ transform: 'translateY(0)', opacity: 1 })),
+        animate('500ms ease-out', style({ transform: 'translateY(0)', opacity: 1 })),
       ])
     ])
   ]
