735154924f365003f53eecc6adad225cedc88ed5
Soham Chitale <chitalesoham106@gmail.com>
2024-12-15 22:57:24 +0530

    explore page changes

--- client/src/app/components/explore/explore.component.ts
+++ client/src/app/components/explore/explore.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { GetCardService } from '../../services/get-card.service';
+import { HttpClient } from '@angular/common/http';
+import { jwtDecode } from 'jwt-decode';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.css'
 })
-export class ExploreComponent {
-
+export class ExploreComponent implements OnInit {
+  ngOnInit(): void {
+    this.getAllCards();
+  }
+  constructor(private getCardService: GetCardService, private http: HttpClient) { }
+  cardList: any[] = [];
+  getAllCards() {
+    const token = localStorage.getItem('jwttoken');
+    const decodedToken: any = token?.toString() && jwtDecode(token);
+    const user_id = decodedToken.user_id;
+    this.getCardService.getCards(user_id).subscribe({
+      next: (data) => {
+        this.cardList = data.cards;
+        console.log('cards fetched successfully', this.cardList);
+      },
+      error: (err) => {
+        console.log('error fetching', err);
+      }
+    })
+  }
 }
