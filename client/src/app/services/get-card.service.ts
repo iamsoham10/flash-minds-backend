a9a11bc51b22ca0b243a1cdd29760ff6edb5423b
Soham Chitale <chitalesoham106@gmail.com>
2024-12-17 19:46:13 +0530

    implemented caching

--- client/src/app/services/get-card.service.ts
+++ client/src/app/services/get-card.service.ts
@@ -1,16 +1,26 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetCardService {
+  private cardCache: any[] | null = null;
   getCardsAPI = "http://localhost:3000/api/cards/readcard";
   constructor(private http: HttpClient) { }
 
   getCards(user_id: string): Observable<any> {
-    const params = new HttpParams().set('user_id', user_id);
-    return this.http.get(this.getCardsAPI, { params });
+    if (this.cardCache) {
+      console.log('returning cached card data');
+      return of({ cards: this.cardCache });
+    }
+    return this.http.get(`${this.getCardsAPI}`, { params: new HttpParams().set('user_id', user_id) })
+      .pipe(tap((response: any) => {
+        this.cardCache = response.cards;
+      }));
+  }
+  clearCache() {
+    this.cardCache = null;
   }
 }
