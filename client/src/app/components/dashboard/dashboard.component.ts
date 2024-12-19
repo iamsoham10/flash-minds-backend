0bfe3ea6aaab84bbf4ed884e6d15a496dc52ed37
Soham Chitale <chitalesoham106@gmail.com>
2024-12-19 17:59:43 +0530

    dashboard page fixed

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -2,50 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { GetCardService } from '../../services/get-card.service';
 import { HttpClient } from '@angular/common/http';
 import { jwtDecode } from 'jwt-decode';
-import { Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
   cardList: any[] = [];
+  filteredCardList: any[] = [];
+  CurrentUserName: string = '';
+  subject: string = '';
 
   ngOnInit(): void {
     this.getAllCards();
   }
-  constructor(private getCardsService: GetCardService, private http: HttpClient, private router: Router) { }
+  constructor(private getCardsService: GetCardService, private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
+
   getAllCards() {
     const token = localStorage.getItem('jwttoken');
-    if (!token) {
-      this.router.navigate(['/auth']);
-    }
-    const decoded: any = token?.toString() && jwtDecode(token);
-    const currentTime = Date.now() / 1000;
-    if (decoded.exp < currentTime) {
-      localStorage.removeItem('jwttoken');
-      console.log('jwt token expired');
-      this.router.navigate(['/auth']);
-    }
-    else {
-      console.log('token is fresh');
-    }
+    const decodedToken: any = token?.toString() && jwtDecode(token);
+    const user_id = decodedToken.user_id;
+    this.CurrentUserName = decodedToken.username;
+    this.getCardsService.getCards(user_id).subscribe({
+      next: (data) => {
+        this.cardList = data.cards;
+        this.getUniqueSubjects(this.cardList);
+        this.subject = this.route.snapshot.paramMap.get('subject') || '';
+        this.filteredCardList = this.cardList.filter(card => card.username === this.CurrentUserName);
+        console.log('Filtered cards: ', this.filteredCardList);
+      },
+      error: (err) => {
+        console.log('error fetching', err);
+      }
+    })
+  }
 
-    // const user_id = decoded.user_id;
-    // this.getCardsService.getCards(user_id).subscribe({
-    //   next: (data) => {
-    //     this.cardList = data.cards;
-    //     console.log("Cards fetched successfully: ", data);
-    //   },
-    //   error: (err) => {
-    //     console.error('Error fetching cards: ', err);
-    //   }
-    // })
+  getUniqueSubjects(cardList: any[]): string[] {
+    const values = [...new Set(cardList.map(card => card.subject))];
+    return values;
+  }
+
+  getCardCountBySubject(subject: string) {
+    return this.cardList.filter(card => card.subject === subject).length;
   }
 
   isMenuOpened: boolean = false;
