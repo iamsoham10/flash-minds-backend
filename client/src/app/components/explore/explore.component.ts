e975dfb08d45fe912234e81a2760476d8c864f42
Soham Chitale <chitalesoham106@gmail.com>
2024-12-20 17:12:26 +0530

    categories filter created

--- client/src/app/components/explore/explore.component.ts
+++ client/src/app/components/explore/explore.component.ts
@@ -20,6 +20,7 @@ export class ExploreComponent implements OnInit {
   }
   constructor(private getCardService: GetCardService, private http: HttpClient) { }
   cardList: any[] = [];
+  categorizedSubjects: { [key: string]: string[] } = {};
   getAllCards() {
     const token = localStorage.getItem('jwttoken');
     const decodedToken: any = token?.toString() && jwtDecode(token);
@@ -28,11 +29,36 @@ export class ExploreComponent implements OnInit {
       next: (data) => {
         this.cardList = data.cards;
         console.log('cards fetched successfully', this.cardList);
+        this.categorizedSubjects = this.groupSubjectsByCategory(this.cardList);
       },
       error: (err) => {
         console.log('error fetching', err);
       }
     })
+  }
+
+  private getCategoryMapping(): { [key: string]: string[] } {
+    return {
+      Engineering: ["Electrical Engineering", "Chemical Engineering", 'Computer Engineering', 'Mechanical Engineering', 'Civil Engineering'],
+      Languages: ["English", 'French', 'German', 'Spanish', 'Japanese'],
+      Science: ['Physics', 'Chemistry', 'Biology'],
+    }
+  }
+
+  private groupSubjectsByCategory(cardList: any[]): { [key: string]: string[] } {
+    const categoryMapping = this.getCategoryMapping();
+    const uniqueSubjects = this.getUniqueSubjects(cardList);
+    const categorizedSubjects: { [key: string]: string[] } = {};
+    Object.keys(categoryMapping).forEach((category) => {
+      categorizedSubjects[category] = uniqueSubjects.filter((subject) =>
+        categoryMapping[category].includes(subject)
+      );
+    });
+    return categorizedSubjects;
+  }
+
+  get categories(): string[] {
+    return Object.keys(this.categorizedSubjects);
   }
 
   getUniqueSubjects(cardList: any[]): string[] {
